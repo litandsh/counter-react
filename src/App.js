41f16b4263f4150406288634ce9bc71d44f8074e
Andrii Litviniuk <litandsh@gmail.com>
2022-10-20 15:25:07 +0200

    refactor: use method map() for creating Button-component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import Button from './components/Button';
 import Counter from './components/Counter';
 
+const texts = ['click 1', 'click 2', 'click 3', 'click 4'];
+
 function App() {
   const [count, setCount] = useState(0);
 
   const incrementCount = () => {
     setCount(count + 1);
-  }
+  };
 
   return (
     <div className="App">
+      
       <Counter count={count} />
-      <Button onClick= {incrementCount} />
-      <Button onClick= {incrementCount} />
-      <Button onClick= {incrementCount} />
-      <Button onClick= {incrementCount} />
+
+      {texts.map((text, index) => {
+        return <Button onClick={incrementCount} text={text} key={index} />;
+      })}
+
     </div>
   );
 }
